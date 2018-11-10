28098691e5eff1b645d443366994cd5a1b4190ef
Elia Anagnostou <39873738+escelia@users.noreply.github.com>
2018-11-10 02:46:06 -0500

    Update box.php

--- box.php
+++ box.php
@@ -9,7 +9,7 @@ $code = intval($_GET['code']);
 $message = "your code is $code";
 $message = wordwrap($message, 70);
 mail('zbatool@brynmawr.edu','Zainab is the bestest',$message);
-header('Location: test.php');  
+header('Location: example/test.php');  
 ?>
 
 </body>
