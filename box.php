9e82efd2c16e7cc9539eee4a15a38356ab3e0de1
Elia Anagnostou <39873738+escelia@users.noreply.github.com>
2018-11-10 02:31:39 -0500

    Update box.php

--- box.php
+++ box.php
@@ -9,6 +9,9 @@ $message = "truer words have never been written";
 $message = wordwrap($message, 70);
 mail('zbatool@brynmawr.edu','Zainab is the bestest',$message);
 header('Location: box.php');
+  
+$code = intval($_GET['code']);   
+print "code is $code";
 ?>
 
 </body>
