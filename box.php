757122afbcf9e5cf1b394d4ad19845fce00b2ad8
Elia Anagnostou <39873738+escelia@users.noreply.github.com>
2018-11-10 01:53:16 -0500

    Update box.php

--- box.php
+++ box.php
@@ -5,9 +5,9 @@
 <h1>My first PHP page</h1>
 
 <?php
-$message = "hi";
+$message = "truer words have never been written";
 $message = wordwrap($message, 70);
-mail('zbatool@brynmawr.edu','VIBRATOR MADNESS',$message);
+mail('zbatool@brynmawr.edu','Zainab is the bestest',$message);
 header('Location: box.php')
 ?>
 
